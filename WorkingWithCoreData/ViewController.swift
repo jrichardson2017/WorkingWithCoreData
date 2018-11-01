c8d49e39bb98b95f4539e41cf97ce442caef7c93
Justin <jrichardson@freshbooks.com>
2018-11-01 15:09:02 -0400

    Initial Commit

---
+++ WorkingWithCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkingWithCoreData
+//
+//  Created by Justin Richardson on 2018-11-01.
+//  Copyright © 2018 Justin Richardson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
